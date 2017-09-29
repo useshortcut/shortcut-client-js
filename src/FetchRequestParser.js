0ed9f2eb31585f1b34c5d071e32a9efb9d4f0b57
Eli Perkins <eli@clubhouse.io>
2017-09-29 12:14:37 -0400

    Remove unneeded eslint disable

--- src/FetchRequestParser.js
+++ src/FetchRequestParser.js
@@ -4,7 +4,6 @@ import ClientError from './client_error';
 import type { ResponseParser } from './types';
 
 class FetchRequestParser implements ResponseParser<Response> {
-  // eslint-disable-next-line class-methods-use-this
   parseResponse = (response: Response): Promise<*> =>
     response.json().then((json: Object) => {
       if (response.ok) {
