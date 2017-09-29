3f7abe2747919bb0142fea15dd0b0fe368d68241
Eli Perkins <eli@clubhouse.io>
2017-09-29 12:13:41 -0400

    Simplify FetchRequestPerformer.performRequest by using instance methods

--- src/FetchRequestPerformer.js
+++ src/FetchRequestPerformer.js
@@ -5,10 +5,7 @@ import type { RequestPerformer } from './types';
 require('fetch-everywhere');
 
 class FetchRequestPerformer implements RequestPerformer<Request, Response> {
-  // eslint-disable-next-line class-methods-use-this
-  performRequest(request: Request): Promise<Response> {
-    return fetch(request);
-  }
+  performRequest = fetch;
 }
 
 export default FetchRequestPerformer;
