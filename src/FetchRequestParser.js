4d7738bae0c5ded8c18ecb7871ad6c5a3fc94cb5
andjosh <email@andjosh.com>
2018-02-16 15:51:59 -0600

    yarn lint --fix with 2-spaces

--- src/FetchRequestParser.js
+++ src/FetchRequestParser.js
@@ -4,17 +4,17 @@ import ClientError from './client_error';
 import type { ResponseParser } from './types';
 
 class FetchRequestParser implements ResponseParser<Response> {
-    parseResponse = (response: Response): Promise<*> =>
-        response
-            .json()
-            .then((json: Object) => {
-                if (response.ok) {
-                    return json;
-                }
+  parseResponse = (response: Response): Promise<*> =>
+    response
+      .json()
+      .then((json: Object) => {
+        if (response.ok) {
+          return json;
+        }
 
-                return Promise.reject(new ClientError(response, json));
-            })
-            .catch(() => Promise.reject(new ClientError(response, {})));
+        return Promise.reject(new ClientError(response, json));
+      })
+      .catch(() => Promise.reject(new ClientError(response, {})));
 }
 
 export default FetchRequestParser;
