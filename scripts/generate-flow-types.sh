8d0fd579e65785a7ffbd6bd16ccb65141aff34a0
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2019-10-28 17:22:34 -0400

    Refactor generate-flow-types.sh

--- scripts/generate-flow-types.sh
+++ scripts/generate-flow-types.sh
@@ -1,3 +1,15 @@
-# /bin/sh
+#!/usr/bin/env bash
 
-for file in $(find ./lib -name *.d.ts -type f); do flowgen --add-flow-header ${file} -o ${file/.d.ts/.js.flow}; done;
+set -o errexit
+set -o pipefail
+set -u
+
+readonly IFS=$'\n\t'
+
+_main() {
+  for file in $(find ./lib -name *.d.ts -type f); do
+    flowgen --add-flow-header "${file}" -o "${file/.d.ts/.js.flow}"
+  done
+}
+
+_main "$@"
