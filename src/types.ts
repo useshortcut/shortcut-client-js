e2d575060e92cfef0c1fc2b4548b7888033986df
Nicolas Charpentier <nicolas@clubhouse.io>
2021-03-03 09:36:35 -0500

    Update PullRequest type (#96)
    * Update PullRequest type
    
    * s/branch_status/build_status

--- src/types.ts
+++ src/types.ts
@@ -269,23 +269,37 @@ export type Commit = {
   url: string;
 };
 
+export type PullRequestLabel = {
+  color: string;
+  description: string | null;
+  entity_type: string;
+  id: number;
+  name: string;
+};
+
 export type PullRequest = {
   branch_id: number;
   branch_name: string;
+  build_status: string;
   closed: boolean;
   created_at: string;
+  draft: boolean;
   entity_type: string;
   id: number;
+  merged: boolean;
   num_added: number;
   num_commits: number;
   num_modified: number | null;
   num_removed: number;
   number: number;
+  repository_id: number;
+  review_status: string;
   target_branch_id: number;
   target_branch_name: string;
   title: string;
   updated_at: string;
   url: string;
+  vcs_labels: Array<PullRequestLabel> | null;
 };
 
 export type CreateExternalTicketParams = {
