7019c72e8035fa7aa0f2e56b357a06076d2d6549
Mika <mika@mikabytes.com>
2021-09-17 16:36:44 +0200

    deleteStoryComment throws error on 204 No Content (#98)

--- src/FetchRequestParser.ts
+++ src/FetchRequestParser.ts
@@ -4,8 +4,11 @@ import { ResponseParser } from './types';
 class FetchRequestParser implements ResponseParser<Response> {
   public readonly parseResponse = (
     response: Response,
-  ): Promise<Record<string, any>> =>
-    response
+  ): Promise<Record<string, any>> => {
+    if (response.status === 204) {
+      return Promise.resolve({});
+    }
+    return response
       .json()
       .then((json: Record<string, any>) => {
         if (response.ok) {
@@ -15,6 +18,7 @@ class FetchRequestParser implements ResponseParser<Response> {
         return Promise.reject(new ClientError(response, json));
       })
       .catch(() => Promise.reject(new ClientError(response, {})));
+  };
 }
 
 export default FetchRequestParser;
