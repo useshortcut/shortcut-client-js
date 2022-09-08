efcc5bc6edbbc7326d7f30c81decdd37baba2501
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2022-09-08 00:25:06 -0700

    Upgrade to latest axios

--- src/generated/http-client.ts
+++ src/generated/http-client.ts
@@ -13,9 +13,9 @@ import axios, {
   AxiosInstance,
   AxiosRequestConfig,
   AxiosResponse,
+  HeadersDefaults,
   ResponseType,
 } from "axios";
-import * as FormData from "form-data";
 
 export type QueryParamsType = Record<string | number, any>;
 
@@ -86,26 +86,62 @@ export class HttpClient<SecurityDataType = unknown> {
     this.securityData = data;
   };
 
-  private mergeRequestParams(
+  protected mergeRequestParams(
     params1: AxiosRequestConfig,
     params2?: AxiosRequestConfig,
   ): AxiosRequestConfig {
+    const method = params1.method || (params2 && params2.method);
+
     return {
       ...this.instance.defaults,
       ...params1,
       ...(params2 || {}),
       headers: {
-        ...(this.instance.defaults.headers || {}),
+        ...((method &&
+          this.instance.defaults.headers[
+            method.toLowerCase() as keyof HeadersDefaults
+          ]) ||
+          {}),
         ...(params1.headers || {}),
         ...((params2 && params2.headers) || {}),
       },
     };
   }
 
-  private createFormData(input: Record<string, unknown>): FormData {
+  protected stringifyFormItem(formItem: unknown) {
+    if (typeof formItem === "object" && formItem !== null) {
+      return JSON.stringify(formItem);
+    } else {
+      return `${formItem}`;
+    }
+  }
+
+  protected createFormData(input: Record<string, unknown>): FormData {
     return Object.keys(input || {}).reduce((formData, key) => {
       const property = input[key];
-      formData.append(key, property);
+      const propertyContent: Iterable<any> =
+        property instanceof Array ? property : [property];
+
+      for (const formItem of propertyContent) {
+        const isFileType = formItem instanceof Blob || formItem instanceof File;
+        formData.append(
+          key,
+          isFileType ? formItem : this.stringifyFormItem(formItem),
+        );
+      }
+
+      return formData;
+    }, new FormData());
+    return Object.keys(input || {}).reduce((formData, key) => {
+      const property = input[key];
+      formData.append(
+        key,
+        property instanceof Blob
+          ? property
+          : typeof property === "object" && property !== null
+          ? JSON.stringify(property)
+          : `${property}`,
+      );
       return formData;
     }, new FormData());
   }
@@ -125,7 +161,7 @@ export class HttpClient<SecurityDataType = unknown> {
         (await this.securityWorker(this.securityData))) ||
       {};
     const requestParams = this.mergeRequestParams(params, secureParams);
-    const responseFormat = (format && this.format) || void 0;
+    const responseFormat = format || this.format || undefined;
 
     if (
       type === ContentType.FormData &&
@@ -133,10 +169,6 @@ export class HttpClient<SecurityDataType = unknown> {
       body !== null &&
       typeof body === "object"
     ) {
-      requestParams.headers.common = { Accept: "*/*" };
-      requestParams.headers.post = {};
-      requestParams.headers.put = {};
-
       body = this.createFormData(body as Record<string, unknown>);
     }
 
@@ -145,9 +177,6 @@ export class HttpClient<SecurityDataType = unknown> {
       headers: {
         ...(type && type !== ContentType.FormData
           ? { "Content-Type": type }
-          : {}),
-        ...(body instanceof FormData
-          ? { "Content-Type": body.getHeaders()["content-type"] }
           : {}),
         ...(requestParams.headers || {}),
       },
