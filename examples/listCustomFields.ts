b85653827218ce265da206de46717541e6c2e1de
Daniel Gregoire <daniel.gregoire@shortcut.com>
2025-03-07 16:30:56 -0500

    Update example listProjects to listCustomFields
    Since Projects are a deprecated feature of Shortcut, this changes the
    example to one that demonstrates a related feature of Shortcut: custom
    fields.

--- examples/listCustomFields.ts
+++ examples/listCustomFields.ts
@@ -2,4 +2,4 @@ import { ShortcutClient } from '../src';
 
 const shortcut = new ShortcutClient(process.env.SHORTCUT_API_TOKEN); // See https://github.com/useshortcut/shortcut-client-js#how-to-get-an-api-token
 
-shortcut.listProjects().then((response) => console.log(response?.data));
+shortcut.listCustomFields().then((response) => console.log(response?.data));
