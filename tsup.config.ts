df0aa7a770be3a953e06edc111fd469b1c54d340
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-06-06 16:31:34 -0400

    Fix `glob`

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'tsup';
-import pkg from 'glob';
-
-const { glob } = pkg;
+import { globSync } from 'glob';
 
 export default defineConfig({
   // Find all TypeScript files (excluding tests and type definitions)
-  entry: glob.sync('src/**/*.ts', {
+  entry: globSync('src/**/*.ts', {
     ignore: ['src/**/__tests__/**', 'src/**/*.test.ts', 'src/**/*.d.ts'],
   }),
   outDir: 'lib',
